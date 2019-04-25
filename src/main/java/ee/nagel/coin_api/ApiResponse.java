05ee91551d9dcffd7f22e3d424aaefb4782089ee
Mehis <mehis.muldma@ringit.ee>
2019-04-25 19:07:53 +0300

    get min/max prices

--- src/main/java/ee/nagel/coin_api/ApiResponse.java
+++ src/main/java/ee/nagel/coin_api/ApiResponse.java
@@ -17,32 +17,62 @@ import java.util.Map;
 @Service
 public class ApiResponse {
 
-    public void ad(String input) {
+  public void ad(String input) {
 
-        RestTemplate restTemplate = new RestTemplate();
+    RestTemplate restTemplate = new RestTemplate();
 
-      List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
-      //Add the Jackson Message converter
-      MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-      // Note: here we are making this converter to process any kind of response, 
-      // not only application/*json, which is the default behaviour
-      converter.setSupportedMediaTypes(Arrays.asList(MediaType.ALL));
-      messageConverters.add(converter);
-      restTemplate.setMessageConverters(messageConverters);
-        
-      LocalDate start = LocalDate.now().minusDays(30);
-      LocalDate end = LocalDate.now();
-      ResponseEntity<Response> response = restTemplate.getForEntity("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + start + "&end=" + end + "&currency=" + input, Response.class);
-      Map<String, Double> mapOfDatesAndPrices = response.getBody().getBpi();
-      System.out.println();
+    List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+    //Add the Jackson Message converter
+    MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+    // Note: here we are making this converter to process any kind of response, 
+    // not only application/*json, which is the default behaviour
+    converter.setSupportedMediaTypes(Arrays.asList(MediaType.ALL));
+    messageConverters.add(converter);
+    restTemplate.setMessageConverters(messageConverters);
+
+    LocalDate start = LocalDate.now().minusDays(30);
+    LocalDate end = LocalDate.now();
+    ResponseEntity<Response> response = restTemplate.getForEntity("https://api.coindesk.com/v1/bpi/historical/close.json?start=" + start + "&end=" + end + "&currency=" + input, Response.class);
+    Map<String, Double> mapOfDatesAndPrices = response.getBody().getBpi();
+
+    Double maximumPrice = getMaxPrice(mapOfDatesAndPrices);
+    Double minimumPrice = getMinPrice(mapOfDatesAndPrices);
+
+    System.out.println("The maximum price for last month was: " + maximumPrice + " " + input);
+    System.out.println("The minimum price for last month was: " + minimumPrice + " " + input);
+  }
+
+  /**
+   * @param datesAndPrices Map of dates and prices
+   * @return Double maximum price
+   */
+  private Double getMaxPrice(Map<String, Double> datesAndPrices) {
+    Double maxPrice = 0.0;
+    Double[] prices = datesAndPrices.values().toArray(Double[]::new);
+    for (Double price : prices) {
+      if (price > maxPrice) {
+        maxPrice = price;
+      }
     }
-    
-//    private Double getMaxPrice(Map<String, Double> datesAndPrices) {
-//      // get max price from map and return
-//    }
-//    
-//    private Double getMinPrice(Map<String, Double> datesAndPrices) {
-//      // get min price from map and return
-//    }
+    return maxPrice;
+  }
+
+  /**
+   * @param datesAndPrices Map of dates and prices
+   * @return Double minimum price
+   */
+  private Double getMinPrice(Map<String, Double> datesAndPrices) {
+    Double minPrice = Double.MAX_VALUE;
+
+    Double[] prices = datesAndPrices.values().toArray(Double[]::new);
+    for (Double price : prices) {
+      if (price < minPrice) {
+        minPrice = price;
+      }
+    }
+    return minPrice;
+
+  }
+
 }
 
